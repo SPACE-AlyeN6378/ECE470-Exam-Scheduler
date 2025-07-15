d7a6fb86d633572aa3bbf54cd5be9a7587a0d0a3
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2025-07-15 15:10:12 -0700

    New tests added

--- genetic_algorithm/list_contains.m
+++
@@ -1,9 +0,0 @@
-function found = list_contains(cellArray, target)
-    found = false;
-    for i = 1:length(cellArray)
-        if isequal(cellArray{i}, target)
-            found = true;
-            break;
-        end
-    end
-end
--- tests/test.m
+++ tests/test.m
@@ -1,6 +1,14 @@
-addpath("genetic_algorithm/")
+addpath("genetic_algorithm/");
+addpath("genetic_algorithm/fitness/")
+addpath('dataset/');
 
-scores = [5, 12, 55, 104, 6, 9];
-selected_index = inverted_roulette_selection(scores);
-disp(['Selected index: ', num2str(selected_index), ...
-      ', value: ', num2str(scores(selected_index))]);
+% scores = [5, 12, 55, 104, 6, 9];
+% selected_index = inverted_roulette_selection(scores);
+% disp(['Selected index: ', num2str(selected_index), ...
+%       ', value: ', num2str(scores(selected_index))]);
+
+students = student_data();
+population = init_population(8);
+fitness_scores = cellfun(@(comb) fitness(comb, students), population)
+
+% fitness(population, students)
